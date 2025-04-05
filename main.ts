70dd8215d5d275d747d02fbb645e6c6ddee8178e
NathanDSanta <natan.d.santa@gmail.com>
2025-04-05 15:03:25 +0200

    working with https requests

--- main.ts
+++ main.ts
@@ -1,84 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Menu, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import https from 'https';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface GithubOverhaulSettings {
+	Github_username: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GithubOverhaulSettings = {
+	Github_username: 'username'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GithubOverhaul extends Plugin {
+	settings: GithubOverhaulSettings;
 
+  // Configure resources needed by the plugin.
 	async onload() {
-		await this.loadSettings();
+    // console.log("Start loading plugin");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    await this.loadSettings();
+    
+    this.addCommand({
+      id: 'print-greeting-to-console',
+      name: 'Print greeting to console',
+      callback: () => {
+        console.log('Greeting');
+      },
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.addCommand({
+      id: 'editor-sample',
+      name: 'editor-sample-command',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        const sel = editor.getSelection();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        console.log(`You have selected: ${sel}`);
+        
+      },
+    });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    this.addRibbonIcon('github', 'Open menu', (event) => {
+      const menu = new Menu();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+      menu.addItem((item) => 
+        item
+          .setTitle('Sync')
+          .setIcon('folder-sync')
+          .onClick(() => {
+          new Notice('Copied');
+        })
+      );
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+      menu.addItem((item) =>
+        item
+          .setTitle('Paste')
+          .setIcon('paste')
+          .onClick(() => {
+          new Notice('Pasted');
+        })
+      );
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+      menu.showAtMouseEvent(event);
+    });
+
+    this.registerEvent(
+      this.app.workspace.on('file-menu', (menu, file) => {
+        menu.addItem((item) => {
+          item
+            .setTitle('Comment')
+            .setIcon('message-square')
+            .onClick(async () => {
+              let response = await this.getComments("NathanDSanta", "githubTesting", 1);
+              console.log("Button getComments", response);
+            })
+        });
+
+        menu.addItem((item) => {
+          item
+            .setTitle('getComments')
+            .setIcon('arrow-down-to-line')
+            .onClick(async () => {
+              let response = await this.getComments("NathanDSanta", "githubTesting", 1);
+              console.log("Button getComments", response);
+            })
+        });
+      })
+    );
+    
+    this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
-	onunload() {
+  // Release any resources configured by the plugin.
+	async onunload() {
+    // console.log("... ");
 
 	}
 
@@ -89,6 +103,94 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+    async fetchAPI(url: string): Promise<any> {
+        try {
+            const response = await fetch(url, {
+                method: "GET", // or POST, PUT, DELETE, etc.
+                headers: {
+                    "Content-Type": "application/json",
+                    // Add additional headers if needed
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching API:", error);
+            return null;
+        }
+    }
+
+  async request(options: any, postData?: string) : Promise<any>{
+    const req = https.request(options, (res: any) => {
+      let data = "";
+
+      res.on("data", (chunk: Buffer) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        console.log("POST Response:", JSON.parse(data));
+        return JSON.parse(data);
+      });
+
+    });
+
+    req.on("error", (error: Error) => {
+      console.error("Error:", error);
+    });
+
+    if(postData !== undefined){
+      req.write(postData);
+    }
+    req.end();
+
+  };
+
+  async getComments(owner: string, repo: string, issue: number) : Promise<any>{
+    const personalAccessToken = "";
+    const options = {
+      hostname: "api.github.com",
+      path: `/repos/${owner}/${repo}/issues/${issue}/comments`,
+      method: "GET",
+      headers: {
+        "Authorization": `token ${personalAccessToken}`,
+        "User-Agent": "Node.js"
+      },
+
+    }
+
+    return await this.request(options);
+  }
+
+  async addComment(comment: string, owner: string, repo: string, issue: number) : Promise<any>{
+    const personalAccessToken = "";
+
+    const postData = JSON.stringify({
+      body: comment,
+    });
+
+    const options = {
+      hostname: "api.github.com",
+      path: `/repos/${owner}/${repo}/issues/${issue}/comments`,
+      method: "POST",
+      headers: {
+        "Authorization": `token ${personalAccessToken}`,
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(postData),
+        "User-Agent": "Node.js"
+      },
+
+
+    };
+
+    return await this.request(options,postData);
+  }
 }
 
 class SampleModal extends Modal {
@@ -108,9 +210,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: GithubOverhaul;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GithubOverhaul) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +223,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Github Username')
+			.setDesc('Your Github username')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.Github_username)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.Github_username = value;
 					await this.plugin.saveSettings();
 				}));
 	}
